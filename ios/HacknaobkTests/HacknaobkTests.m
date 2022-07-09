850a5c393c96f8d8a9b0490799fcaa3e0efac962
james.nguyen <thaocrop@gmail.com>
2022-07-09 16:47:17 +0700

    update

--- ios/HacknaobkTests/HacknaobkTests.m
+++ ios/HacknaobkTests/HacknaobkTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface hackBkTiengNhatAppTests : XCTestCase
+@interface HacknaobkTests : XCTestCase
 
 @end
 
-@implementation hackBkTiengNhatAppTests
+@implementation HacknaobkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
